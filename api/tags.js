a8aa1711561c03968a263e6a80d25a3164b202ba
HalParadis <halparadis@gmail.com>
2023-05-19 02:07:51 -0700

    Completed GET /api/tags/:tagName/posts route

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => {
   console.log('A request is being made to /tags');
@@ -10,6 +10,17 @@ tagsRouter.use((req, res, next) => {
 tagsRouter.get('/', async (req, res) => {
   const tags = await getAllTags();
   res.send({ tags });
-})
+});
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  const { tagName } = req.params;
+  try {
+    const posts = await getPostsByTagName(tagName.replace('%23', '#'));
+    res.send({ posts });
+  }
+  catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 module.exports = tagsRouter;
